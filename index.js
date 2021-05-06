8d063a5209ebd5a74481614ab5adb766d5ad9b4e
Graziele Dantas <elentary@gmail.com>
2021-05-06 16:52:47 -0300

    fixed

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const mongoose = require("mongoose");
 const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
 const data = require("./data");
-const { init } = require("./models/Recipe.model");
 
 const MONGODB_URI = "mongodb://localhost:27017/recipe-app";
 
@@ -21,10 +20,8 @@ mongoose
     return Recipe.deleteMany();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
-
     //Iteration II
-    const result = Recipe.create({
+    Recipe.create({
       title: "Bolinho de Arroz",
       level: "Amateur Chef",
       ingredients: [
@@ -45,31 +42,35 @@ mongoose
       duration: 30,
       creator: "Vovózinha",
       created: "1800/05/05",
+    }).then((result) => {
+      console.log("Receita Criada - ", result);
+
+      //Iteration III
+      //db.collectionName.insertMany(json, function(err,result)
+      Recipe.insertMany(data).then((allRecipes) => {
+        console.log("Todas as Receitas - ", allRecipes);
+
+        //Iteration IV
+        Recipe.findOneAndUpdate(
+          { title: "Rigatoni alla Genovese" },
+          { $set: { duration: 100 } },
+          { new: true }
+        ).then((updateRecipe) => {
+          console.log("Recipe Atualizada - ", updatedRecipe);
+
+          //Iteration V
+          Recipe.deleteOne({
+            title: "Carrot Cake",
+          }).then((deletedRecipe) => {
+            console.log("Recipe Deletada - ", deleteRecipe);
+
+            //Iteration VI
+            mongoose.connection.close();
+          });
+        });
+      });
     });
-    console.log("Receita Criada - ", result);
-
-    //Iteration III
-    //db.collectionName.insertMany(json, function(err,result)
-    const allRecipes = Recipe.insertMany(data);
-    console.log("Todas as Receitas - ", allRecipes);
-
-    //Iteration IV
-    const updatedRecipe = Recipe.findOneAndUpdate(
-      { title: "Rigatoni alla Genovese" },
-      { $set: { duration: 100 } },
-      { new: true }
-    );
-    console.log("Recipe Atualizada - ", updatedRecipe);
-
-    //Iteration V
-    const deleteRecipe = Recipe.deleteOne({
-      title: "Carrot Cake",
-    });
-    console.log("Recipe Deletada - ", deleteRecipe);
-
-    //Iteration VI
-    mongoose.connection.close();
-
-  }).catch((error) => {
+  })
+  .catch((error) => {
     console.error("Error connecting to the database", error);
   });
